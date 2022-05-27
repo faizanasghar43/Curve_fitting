67b70f3dda388eae42dad22769c69b4faaa289aa
faizanasghar43 <81610362+faizanasghar43@users.noreply.github.com>
2022-05-27 15:26:35 +0500

    Curve fitted

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 # importing the required module
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
 import numpy as np
 import scipy.stats as stats
 
@@ -28,24 +27,18 @@ y = [5, 6, 7, 6, 9, 8, 7, 10, 12, 12]
 # x = [46 ,65, 53 ,38 ,61 ,89 ,59 ,60 ,73]
 # y = [940, 790, 910, 1020, 540, 340, 810, 720, 830]
 
-slope, intercept, r_value, p_value, std_err = linregress(x, y)
-print("Slope =", slope)
-print("Intercept =", intercept)
-print("r_value", r_value)
-print("p_value", p_value)
-print("std_error in % ", std_err*100, "%")
-r = sum_fun(stats.zscore(x)*stats.zscore(y), 9)
+
+r = sum_fun(stats.zscore(x)*stats.zscore(y), 9)  # r_values using z_scores
 r = r/9
 print("r = ", r)
-div_std = np.std(y)/np.std(x)
-slide_slope = r*div_std
+div_std = np.std(y)/np.std(x)      # std being divided to multiply with r_value and obtain slope
+slope = r*div_std
 
-intercept_slide = np.mean(y) - slide_slope*np.mean(x)
+intercept = np.mean(y) - slope*np.mean(x)
 
-true_value_error_finder(x, y, slide_slope, intercept_slide)
+true_value_error_finder(x, y, slope, intercept)
 
-x = np.array(x)
-y = np.array(y)
+
 plt.plot(x, y, 'o')
 slide_slope, intercept_slide = np.polyfit(x, y, 1)
 plt.plot(x, slide_slope*x + intercept_slide)
